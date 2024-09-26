521fa42c649bfaed00fe682f634a111888bc274f
Kundan2099 <121600019+Kundan2099@users.noreply.github.com>
2024-09-26 21:33:35 +0530

    api status

---
+++ app/Http/Controllers/Admin/Api/ApiController.php
@@ -0,0 +1,34 @@
+<?php
+
+// namespace App\Http\Controllers\Admin;
+
+// response([
+//     'status' => true,
+//     'message' => "Status successfully Created", //Created
+//     'data' => $admin
+// ], 201);
+
+//      response([
+//         'status' => true,
+//         'message' => "Status successfully updated", //ok
+//         'data' => $admin
+//     ], 200);
+
+//     response([
+//         'status' => true,
+//         'message' => "Status successfully redirection", //redirection
+//         'data' => $admin
+//     ], 300); 
+
+//     return response([
+//         'status' => false,
+//         'message' => "An error occcured", //internal server error
+//         'error' => $exception->getMessage()
+//     ], 500);
+
+//     response([
+//         'status' => true,
+//         'message' => "Status successfully redirection", //Bad Request
+//         'data' => $admin
+//     ], 400); 
+
